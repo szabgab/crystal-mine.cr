d851b9bd49c7d3efc80de43e8597b647e2cdf8fc
Gabor Szabo <gabor@szabgab.com>
2021-05-22 11:42:35 +0300

    move fixture to helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,23 @@
 require "spec-kemal"
 require "../src/webapp"
+
+require "file_utils"
+
+def fixture(cleanup = true)
+  tempfile = File.tempname
+  #puts tempfile
+  begin
+    # load data into temporary database
+    # TODO eliminate the need for the sqlite3 cli
+    res = system("sqlite3 #{tempfile} < spec/data.sql")
+    if ! res
+      raise "Could not create fixture"
+    end
+    ENV["MINE_DB"] = tempfile
+    yield
+  ensure
+    if cleanup
+       FileUtils.rm_rf(tempfile)
+    end
+  end
+end
--- spec/test_spec.cr
+++ spec/test_spec.cr
@@ -1,24 +1,4 @@
 require "./spec_helper"
-require "file_utils"
-
-def fixture(cleanup = true)
-  tempfile = File.tempname
-  #puts tempfile
-  begin
-    # load data into temporary database
-    # TODO eliminate the need for the sqlite3 cli
-    res = system("sqlite3 #{tempfile} < spec/data.sql")
-    if ! res
-      raise "Could not create fixture"
-    end
-    ENV["MINE_DB"] = tempfile
-    yield
-  ensure
-    if cleanup
-       FileUtils.rm_rf(tempfile)
-    end
-  end
-end
 
 describe "My Kemal App" do
   it "renders /" do
