eae524b6b55838b067fb5b2b954ebdff0152cc05
Gabor Szabo <gabor@szabgab.com>
2021-06-03 15:04:48 +0300

    code to update the files needed for the tests

--- spec/update.cr
+++ spec/update.cr
@@ -1,14 +1,16 @@
 require "./spec_helper"
-
+require "../src/mine/db"
 # updated the data for the tests
 
 no_db_fixture(cleanup: true) do
+    puts ENV["MINE_DATA"]
     puts ENV["MINE_DB"]
 
     stdout, stderr, exit_code = capture(CRYSTAL, ["src/mine.cr", "--repos", "spec/repos.txt" ])
     stderr.should eq ""
     stdout.should eq ""
     exit_code.should eq 0
+    File.write("spec/data.json", get_all().to_json)
 
     `echo .dump | sqlite3 #{ENV["MINE_DB"]} > spec/data.sql`
 end
