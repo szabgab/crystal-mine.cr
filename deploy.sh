4eb1c74b6b30427cbe6a80967980a74112b9b9ca
Gabor Szabo <gabor@szabgab.com>
2021-06-08 08:19:51 +0300

    script to generate bulma.css

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/home/gabor/docker-compose/bin
 
 git pull
-docker run --rm -it -v$(pwd):/opt --workdir=/opt node:14.15 npm install && npm run css-build
+docker run --rm -it -v$(pwd):/opt --workdir=/opt node:14.15 bash generate_bulma.sh
 docker-compose build
 docker-compose stop --time 0
 docker-compose up --detach --time 0 --remove-orphans
